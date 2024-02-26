d037eb1f7ca52cc53e500805ef5bc08c166e429f
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2024-02-26 14:49:37 +0300

    :sparkles: Clean up

--- Services/NotificationService/Helpers/SendEmail.cs
+++ Services/NotificationService/Helpers/SendEmail.cs
@@ -9,7 +9,7 @@ public class SendEmail{
     public static async Task<bool> SendEmailMethod(string email, string subject, string messageBody) {
         await Task.Run(() => {
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("TaskManagerTeam", "mahdertekola@gmail.com"));
+            message.From.Add(new MailboxAddress("TaskManagerTeam"));
             message.To.Add(new MailboxAddress("", email));
             message.Subject = subject;
             message.Body = new TextPart("html"){Text = messageBody};
@@ -17,7 +17,7 @@ public class SendEmail{
          using (var client = new SmtpClient())
         {
             client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-            client.Authenticate("noreply.TaskManager1@gmail.com", "zfxa tlkz vzcd sius ");
+            client.Authenticate("noreply.TaskManager1@gmail.com", "password ");
             client.Send(message);
             client.Disconnect(true);
         }
